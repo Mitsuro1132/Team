1c12b09ce64df971490e071359a1fdb611d8ffe8
Sasha Stognii <mitsuro101@gmail.com>
2025-05-21 11:15:25 +0300

    finishTask

--- index.py
+++ index.py
@@ -1,1 +1,6 @@
-print(21)
+class bank:
+    credit_person = None
+    def __init__(self,name,email,count_dollar):
+        self.name = name
+        self.email = email
+        self.count_dollar = count_dollar
