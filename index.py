dfa47eb6cf561bdec524ab8fbe8a65c860c5ba41
Yulian Doinov <yulikdoynov@gmail.com>
2025-05-21 11:24:42 +0300

    yulianchange

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
 class bank:
     credit_person = None
+    __bankname = 'PrivatBank'
+    customer_goods = []
     def __init__(self,name,email,count_dollar):
         self.name = name
         self.email = email
         self.count_dollar = count_dollar
+
+    @classmethod
+    def get_bank_name(cls):
+        print(cls.__bankname)
